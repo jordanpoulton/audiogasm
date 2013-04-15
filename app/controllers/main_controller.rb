7f37953a9723a4f75575922e01823915c3f7606c
Michelle <yhzgirl@gmail.com>
2013-04-15 12:36:48 +0100

    initial rails structure

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,7 @@
+class MainController < ApplicationController
+
+  def home
+  end
+
+end
+ 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audiogasm::Application.config.secret_token = 'dcf123845dcc8c7487745bed4065b9aa1d8d9bff5c15f41b1653aed47f1e98e0625bba614ce22393f4a2ae80cb374293a3953068664a9388ce5294e26c476d28'
