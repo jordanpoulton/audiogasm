97c2dcae106eca2392e62fdeb110b63c5db89681
Ruben Kostucki <rubenkostucki@gmail.com>
2013-04-15 16:34:19 +0100

    installed Songkick Ruby gem, waiting to get the API key

--- app/models/song_kick_api.rb
+++ app/models/song_kick_api.rb
@@ -1,7 +1,11 @@
+require 'songkickr'
+remote = Songkickr::Remote.new API_KEY
+
 class SongKickApi
 
   def self.area_id_for_location(location)
-
+    @location = location
+    metro_area_id = @location.values_at(:id)
   end
 
 end
