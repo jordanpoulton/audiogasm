fe885a2dfd080ab7c7704760eebdcffb0aa9b702
Michelle <yhzgirl@gmail.com>
2013-04-16 17:28:10 +0100

    update tests and models for EchonestAPI

--- app/models/echonest_api.rb
+++ app/models/echonest_api.rb
@@ -9,12 +9,11 @@ class EchonestApi
   API_KEY = "SYT5EOFXZFDBJFV2P"
   # attr_accessible :title, :body
 
-  def self.get_artist_name(artist_id)
-    http = Curl.get("http://developer.echonest.com/api/v4/artist/profile?api_key=#{API_KEY}&id=#{artist_id}&format=json")
-    json_hash = JSON.parse(http.body_str)
-    json_hash['response']['artist']['name']
-  end
-
+  # def self.get_artist_name(artist_id)
+  #   http = Curl.get("http://developer.echonest.com/api/v4/artist/profile?api_key=#{API_KEY}&id=#{artist_id}&format=json")
+  #   json_hash = JSON.parse(http.body_str)
+  #   json_hash['response']['artist']['name']
+  # end
 
   def self.get_artist_genres_by_id(artist_id)
     http = Curl.get("http://developer.echonest.com/api/v4/artist/terms?api_key=#{API_KEY}&id=#{artist_id}&format=json")
@@ -22,20 +21,20 @@ class EchonestApi
     json_hash['response']['terms'].map{|e| e["name"] }
   end
 
-  def self.get_artist_genres_by_name(artist_name)
-    http = Curl.get("http://developer.echonest.com/api/v4/artist/terms?api_key=#{API_KEY}&name=#{artist_name}&format=json")
-    json_hash = JSON.parse(http.body_str)
-    json_hash['response']['terms'].map{|e| e["name"] }
-  end
+  # def self.get_artist_genres_by_name(artist_name)
+  #   http = Curl.get("http://developer.echonest.com/api/v4/artist/terms?api_key=#{API_KEY}&name=#{artist_name}&format=json")
+  #   json_hash = JSON.parse(http.body_str)
+  #   json_hash['response']['terms'].map{|e| e["name"] }
+  # end
 
-  def self.get_artist_genres_by_weight(artist_name, weight)
-    http = Curl.get("http://developer.echonest.com/api/v4/artist/terms?api_key=#{API_KEY}&name=#{artist_name}&format=json")
+  def self.get_artist_genres_by_weight(artist_id, weight)
+    http = Curl.get("http://developer.echonest.com/api/v4/artist/terms?api_key=#{API_KEY}&name=#{artist_id}&format=json")
     json_hash = JSON.parse(http.body_str)
     json_hash['response']['terms'].select{|w| w["weight"] > weight }.map{|e| e["name"] }
   end
 
-  def self.check_artist_is_of_genre(artist_name, genre)
-    array = get_artist_genres_by_name(artist_name)
+  def self.check_artist_is_of_genre(artist_id, genre)
+    array = get_artist_genres_by_id(artist_id)
     array.each { |item| return true if item == genre}
     false
   end
@@ -51,3 +50,4 @@ end
 
 # artist = Echonest::Artist.new('SYT5EOFXZFDBJFV2P', nil,  [{:catalog => 'artists', :foreign_id => '5380281'}])
 
+# http = Curl.get("http://developer.echonest.com/api/v4/artist/terms?api_key=#{API_KEY}&name=#{artist_name}&format=json")
--- test/unit/song_kick_api_test.rb
+++ test/unit/song_kick_api_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SongKickApiTest < ActiveSupport::TestCase
 
-  test 'can get metro area id for london' do
-    assert_equal 24426, SongKickApi.area_id_for_location("London")
-  end
+  # test 'can get metro area id for london' do
+  #   assert_equal 24426, SongKickApi.area_id_for_location("London")
+  # end
 end
