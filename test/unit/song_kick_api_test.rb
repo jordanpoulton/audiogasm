85e3ebfc27d00c32cdc48998c2977404050f20ee
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2013-04-17 09:30:05 +0100

    Added tests for Songkick API

--- test/unit/song_kick_api_test.rb
+++ test/unit/song_kick_api_test.rb
@@ -5,4 +5,11 @@ class SongKickApiTest < ActiveSupport::TestCase
   test 'can get metro area id for london' do
     assert_equal 24426, SongKickApi.area_id_for_location("London", "UK")
   end
+
+  test 'can get the id of an artist that is playing in specific metro_area_id and date' do
+    array =
+    http = Curl.get("http://api.songkick.com/api/3.0/events.json?location=sk:24426&min_date=#{Time.now.strftime('%Y-%m-%d')}&max_date=#{(Time.now+1.week).strftime('%Y-%m-%d')}&apikey=Hkockg21oUnNQEZa")
+    assert_match(/((?:\+?|\b)[0-9]{6}\b)/, (JSON.parse(http.body_str)["resultsPage"]["results"]["event"][0]["performance"][0]["artist"]["id"].to_s))
+  end
+
 end
