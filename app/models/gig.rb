3fd6b885a9316c42a1f27b6c1b32a9df80d8b2a8
Michelle <yhzgirl@gmail.com>
2013-04-19 13:48:17 +0100

    add gig model

---
+++ app/models/gig.rb
@@ -0,0 +1,16 @@
+class Gig
+
+  attr_reader :artist_id, :location, :date, :venue
+
+  def initialize(artist_id, location, date, venue)
+    @artist_id = artist_id
+    @location = location
+    @date = date
+    @venue = venue
+  end
+
+end
+
+  http = Curl.get("http://api.songkick.com/api/3.0/events.json?location=sk:24426&min_date=2013-04-18&max_date=2013-04-19&page=1&per_page=10&apikey=Hkockg21oUnNQEZa")
+  JSON.parse(http.body_str)["resultsPage"]["results"]["event"][0]["venue"]["displayName"]
+  Gets the venue name
---
+++ test/factories/gig_factory.rb
@@ -0,0 +1,10 @@
+class GigFactory
+
+
+
+  def self.valid_gig
+    Gig.new(:artists => 'radiohead')
+    
+  end
+
+end
