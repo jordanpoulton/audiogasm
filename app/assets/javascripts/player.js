84a291328c3099289884e577982c43bd8211256a
Rich Wisniewski <richwisniewski@gmail.com>
2013-04-21 22:39:34 +0100

    added slideToggle function for form on landing page

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -41,18 +41,20 @@
             console.log("transitioning to slide ", currentSlide);
         };
 
-        
-    });
-// $('.album').on('mouseenter', function (){
-//     $('.player').fadeIn();
-//     $('.playing-text').fadeIn();
-//   });
 
-// $('.album').on('mouseleave', function (){
-//     $('.player').fadeOut();
-//     $('.playing-text').fadeOut();
+    });
+$('.album').on('mouseenter', function (){
+    $('.player').fadeIn();
+    $('.playing-text').fadeIn();
+  });
+
+$('.album').on('mouseleave', function (){
+    $('.player').fadeOut();
+    $('.playing-text').fadeOut();
    
-//   });
+  });
+
+
 })();
 
 // $(function() {
