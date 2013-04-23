4390ce2d3d6985eb6dda2f34452a3ea5fb96060f
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2013-04-23 12:33:43 +0100

    working end to end

--- app/assets/javascripts/main_home.js
+++ app/assets/javascripts/main_home.js
@@ -1,5 +1,9 @@
 $(function() {
   $( ".date_input input" ).datepicker();
+  $( "#format" ).change(function() {
+      $( ".date_input input" ).datepicker( "option", "dateFormat", $( this ).val() );
+    });
+  });
 });
 
 
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,25 +1,26 @@
 class Gig
 
-  attr_reader :songkick_artist_id, :location, :date, :venue
+  attr_reader :artist_id, :location, :date, :venue
 
-  def initialize(songkick_artist_id, location, date, venue)
-    @songkick_artist_id = songkick_artist_id
+  def initialize(artist_id, location, date, venue)
+    @artist_id = artist_id
     @location = location
     @date = date
     @venue = venue
   end
 
   def song
-    artist = @songkick_artist_id.shuffle.first
+    artist = @artist_id.shuffle.first
     SongProvider.get_song_from_rdio(artist)
   end
 
   def self.find(location, from, to, genre)
     gigs = GigInfoProvider.new(location, from, to).get_upcoming_gigs
-    gigs.detect {|gig| gig.songkick_artist_id.detect{ |artist| ArtistFilterInfoProvider.check_artist_is_of_genre(artist, genre)}}
+    gigs.detect {|gig| gig.check_gig_is_valid(genre) }
+  end
 
-    # upcoming_gigs = find_upcoming_gigs
-    # filter_by_genre(upcoming_gigs)
+  def check_gig_is_valid(genre)
+    self.artist_id.any? {|artist| ArtistFilterInfoProvider.check_artist_is_of_genre(artist, genre)}
   end
 
 end
