57dc3e0d3df632e630a8a09755783854c04cc4e7
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2013-04-25 09:55:24 +0100

    Artist name showing on show page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
 
   def show
     @gig = Gig.find(params[:location].downcase, params[:from], params[:to], params[:genre].downcase)
+    @genre = params[:genre].downcase
     @song = @gig.song
+    @artist = ArtistFilterInfoProvider.get_artist_name(@gig.artist)
   end
 end
--- app/models/artist_filter_info_provider.rb
+++ app/models/artist_filter_info_provider.rb
@@ -5,31 +5,42 @@ class ArtistFilterInfoProvider
 
 
   def self.get_artist_genres(artist_id)
-      genres_from(
-        terms_in(
-          parsed_api_call(artist_id)))
-    rescue
-      "Couldn't get artist genres"
+    genres_from(
+      terms_in(
+    parsed_api_call(artist_id)))
+  rescue
+    "Couldn't get artist genres"
+  end
+
+  def self.get_artist_name(artist_id)
+    http = Curl.get("http://developer.echonest.com/api/v4/artist/profile?api_key=#{ECHONEST_API_KEY}&id=songkick:artist:#{artist_id}&format=json")
+    json_hash = JSON.parse(http.body_str)
+    debugger
+    if json_hash["response"]["status"]["message"] == "Success"
+      json_hash['response']['artist']['name']
+    else
+      "Couldn't get artist name"
+    end
   end
 
   private
 
   def self.is_artist_valid?(artist_id, genre)
-      puts artist_id
-      artist_genres = get_artist_genres(artist_id)
-      artist_genres.include?(genre)
-    rescue
-      "We were unable to check artist validity"
+    puts artist_id
+    artist_genres = get_artist_genres(artist_id)
+    artist_genres.include?(genre)
+  rescue
+    "We were unable to check artist validity"
   end
 
   def self.parsed_api_call(artist_id)
-      JSON.parse(Curl.get("http://developer.echonest.com/api/v4/artist/terms?api_key=#{ECHONEST_API_KEY}&id=songkick:artist:#{artist_id}&format=json").body_str)
-    rescue
-      "There was a problem with the API call"
+    JSON.parse(Curl.get("http://developer.echonest.com/api/v4/artist/terms?api_key=#{ECHONEST_API_KEY}&id=songkick:artist:#{artist_id}&format=json").body_str)
+  rescue
+    "There was a problem with the API call"
   end
 
   def self.terms_in(api_response)
-      api_response['response']['terms']
+    api_response['response']['terms']
   end
 
   def self.genres_from(api_terms)
